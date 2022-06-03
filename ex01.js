af71842abaefef67d6c0b6dc1f4c35f4d4613a80
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-06-03 03:24:19 -0300

    resolucao do exercicio 01

--- ex01.js
+++ ex01.js
@@ -1,8 +1,20 @@
-const prompt = require('prompt-sync')();
+const prompt = require("prompt-sync")();
 console.clear();
 
-let numero = +prompt('Digite um número: ');
+let numero = +prompt("Digite um número: ");
+
+let qtdNumerosDivisiveis = 0;
 
 for (let i = 1; i <= numero; i++) {
-    console.log(numero,i);
+  let restoDivisao = numero % i;
+  if (restoDivisao === 0) {
+    qtdNumerosDivisiveis++;
+  }
+  //console.log({numero, i, restoDivisao, qtdNumerosDivisiveis});
+}
+
+if (qtdNumerosDivisiveis === 2) {
+  console.log(`O número ${numero} é primo!`);
+} else {
+  console.log(`O número ${numero} não é primo!`);
 }
